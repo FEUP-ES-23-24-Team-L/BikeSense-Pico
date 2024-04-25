83b3a57322dba1166a200874182335d1e0351411
Tiago Amorim <tiago.andre.amorim@gmail.com>
2024-04-26 00:30:20 +0100

    added DataStorageInterface and a MockDataStorage implementation to BikeSense class

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 #! /usr/bin/env micropython
 
 import random as rand
+from src.bikesense.gps import BksGPS
 from src.bikesense.bikesense import (
     SensorInterface,
     ReadingResult,
     BikeSenseBuilder,
+    DataStorageInterface,
 )
-from src.bikesense.gps import BksGPS
 
 
 class DummySensorModule(SensorInterface):
@@ -17,8 +18,37 @@ class DummySensorModule(SensorInterface):
         return ReadingResult("DummySensor", rand.randint(0, 100))
 
 
+class MockDataStorage(DataStorageInterface):
+    data: list[dict]
+
+    def init(self):
+        self.data = list()
+
+    def save(self, data: dict):
+        self.data.append(data)
+
+        # NOTE:  Since this is a mock storage, we will only keep the last 10 readings
+        #        in order to avoid memory issues. Final implementation should save
+        #        data to an SD card.
+        #
+        # TODO:  Look into SD card interfacing with micropython.
+        #        Look into MicroFat file system.
+        if len(self.data) > 10:
+            self.data.pop(0)
+
+    # NOTE: Instead of returning the entire database, the final implementation
+    #       should return the data in chunks (e.g. 10 readings at a time)
+    #       in order to avoid memory issues and facilitate data upload.
+    #
+    # TODO: Maybe adapt the data storage interface to return data in chunks.
+    def read(self) -> list[dict]:
+        return self.data
+
+
 def main():
-    bks = BikeSenseBuilder(BksGPS()).registerSensor(DummySensorModule())
-    bks.connectWifi("ArchBtw", "123arch321").build().run()
+    bks = BikeSenseBuilder(BksGPS(), MockDataStorage())
+    bks.register_sensor(DummySensorModule())
+    bks.connect_wifi("ArchBtw", "123arch321").build().run()
+
 
 main()
--- src/bikesense/bikesense.py
+++ src/bikesense/bikesense.py
@@ -54,36 +54,63 @@ class GPSModuleInterface:
         return ReadingResult("", "")
 
 
+class DataStorageInterface:
+    """
+    Interface class that defines the necessary functions for concrete data storage.
+
+    A single data storage module must necessarily be implemented to instantiate the BikeSense Class.
+    """
+
+    def init(self):
+        pass
+
+    def save(self, data: dict):
+        pass
+
+    def read(self) -> list[dict]:
+        return list()
+
 
 class BikeSenseBuilder:
     wled: m.Pin
     wlan: nw.WLAN
     gps: GPSModuleInterface
+    ds: DataStorageInterface
     sensors: list[SensorInterface]
 
-    def __init__(self, gps: GPSModuleInterface):
+    def __init__(self, gps: GPSModuleInterface, dataStorage: DataStorageInterface):
         self.wlan = nw.WLAN(nw.STA_IF)
         self.wled = m.Pin("LED", m.Pin.OUT)
+
         self.gps = gps
+        self.ds = dataStorage
+
         self.sensors = list()
 
-    def registerSensor(self, s: SensorInterface):
+    def register_sensor(self, s: SensorInterface):
         self.sensors.append(s)
         return self
 
-    def connectWifi(self, ssid: str, password: str):
+    def connect_wifi(self, ssid: str, password: str):
         self.wlan.active(True)
         self.wlan.connect(ssid, password)
         return self
 
     def build(self):
-        self.gps.init()
+        try:
+            self.gps.init()
+            self.ds.init()
+
+        # TODO: handle this scenario better
+        except Exception as e:
+            print(f"Exception during init: {e}")
+            raise e
 
         if self.sensors:
             for sensor in self.sensors:
                 sensor.init()
 
-        return BikeSense(self.wled, self.wlan, self.gps, self.sensors)
+        return BikeSense(self.wled, self.wlan, self.gps, self.ds, self.sensors)
 
 
 class BikeSense:
@@ -94,6 +121,7 @@ class BikeSense:
     wled: m.Pin
     wlan: nw.WLAN
     gps: GPSModuleInterface
+    ds: DataStorageInterface
     sensors: list[SensorInterface]
 
     def __init__(
@@ -101,14 +129,19 @@ class BikeSense:
         wled: m.Pin,
         wlan: nw.WLAN,
         gps: GPSModuleInterface,
+        ds: DataStorageInterface,
         sensors: list[SensorInterface],
     ):
         self.wled = wled
         self.wlan = wlan
         self.gps = gps
+        self.ds = ds
         self.sensors = sensors
 
-    def _wifiIsConnected(self) -> bool:
+    # NOTE: Most likely this function will be moved to a separate module / refactored
+    #       will probably implement the data upload logic with strategy pattern
+    # TODO: Implement handling for the wlan connection status
+    def _wifi_is_connected(self) -> bool:
         if self.wlan.isconnected():
             self.wled.on()
             print(f"ip = {self.wlan.ifconfig()[0]}")
@@ -118,22 +151,30 @@ class BikeSense:
             print(f"wifi status: {self.wlan.status()}")
             return False
 
-    def run(self, loop_delay_ms: int = 1000):
+    def _read_sensors(self) -> dict:
+        return {r.name: r.value for r in [s.read() for s in self.sensors]}
+
+    def _main_loop(self):
+        if self._wifi_is_connected():
+            # TODO: remove this block and implement a proper data upload strategy
+            try:
+                r = urq.get("http://date.jsontest.com")
+                print(f"get response: {r.json()}")
+            except Exception as e:
+                print(f"Exception with get request: {e}")
+
+        gps = self.gps.read()
+        readings = self._read_sensors()
+        readings.update({gps.name: gps.value})
+
+        self.ds.save(readings)
+
+        print(f"readings: {readings}")
+
+    def run(self, loop_period_ms: int = 1000):
         while True:
-            #TODO: change sleep to timer based approach
-            #      Make http related stuff async
-            utime.sleep_ms(loop_delay_ms)
-
-            if self._wifiIsConnected():
-                try:
-                    r = urq.get("http://date.jsontest.com")
-                    print(f"get response: {r.json()}")
-                except Exception as e:
-                    print(f"Exception with get request: {e}")
-
-            gps = self.gps.read()
-            print(f"{gps}")
-
-            for s in self.sensors:
-                reading = s.read()
-                print(f"{reading.name}: {reading.value}\n")
+            start = utime.ticks_ms()
+            self._main_loop()
+            delta = utime.ticks_diff(utime.ticks_ms(), start)
+            print(f"loop time: {delta}ms\n")
+            utime.sleep_ms(max(loop_period_ms - delta, 0))
