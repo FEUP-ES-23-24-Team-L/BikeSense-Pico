361ed589cc3790bdc1a0ccf69505b9d139ca5fbe
dvalnn <tiago.andre.amorim@gmail.com>
2024-05-15 18:00:35 +0100

    noise sensor concrete implementation. Needs calibration

--- cpp/include/noise.h
+++ cpp/include/noise.h
@@ -4,6 +4,15 @@
 #include <interfaces.h>
 
 class NoiseSensor : public SensorInterface {
+  // TODO: Needs calibration
+  const int PIN = 26;
+  const int ADC_BIAS = 0;
+  const int noiseDBReference = 0;
+  const int noiseADCReference = 1;
+
+  const int mvAvgWindowSize = 10;
+  int mvAvgAccumulator = 0;
+
 public:
   void setup() override;
   SensorReading read() override;
--- cpp/src/noise.cpp
+++ cpp/src/noise.cpp
@@ -1,9 +1,19 @@
+#include "pico/time.h"
 #include <Arduino.h>
 #include <noise.h>
 #include <sensorReading.h>
 
-void NoiseSensor::setup() { Serial.println("Mock sensor is setting up..."); }
+void NoiseSensor::setup() { pinMode(PIN, INPUT); }
 
 SensorReading NoiseSensor::read() {
-  return SensorReading().addMeasurement("noise_level", 77);
+  for (int i = 0; i < mvAvgWindowSize; i++) {
+    mvAvgAccumulator += analogRead(PIN);
+    sleep_us(250);
+  }
+
+  int adcReading = mvAvgAccumulator / mvAvgWindowSize - ADC_BIAS;
+  int deltaDB = 20 * log10(adcReading / noiseADCReference);
+  int noiseDB = noiseDBReference + deltaDB;
+
+  return SensorReading().addMeasurement("noise_level", noiseDB);
 }
